aff1cc362d80c6d25f9384ffb8364e65c15374f2
Your Name <you@example.com>
2025-09-30 10:33:23 -0300

    feat: setup inicial de testes concluído

---
+++ tests/PersonalStylistIA.Tests/SimpleTest.cs
@@ -0,0 +1,11 @@
+using System; 
+ 
+namespace PersonalStylistIA.Tests; 
+ 
+public class SimpleTest 
+{ 
+    public void TesteSimples() 
+    { 
+        Console.WriteLine("Teste compilando com sucesso!"); 
+    } 
+} 
